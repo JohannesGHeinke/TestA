fb2714b657ab7f02ae2dc5b4379b07fa80d6e99b
JohannesGHeinke <johannes.heinke@mikavaa.com>
2018-12-05 16:23:20 +0100

    Initial Commit

---
+++ TestA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestA
+//
+//  Created by Johannes Heinke Business on 05.12.18.
+//  Copyright © 2018 Mikavaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
